e258987e7812273575e189bb299f6f8e346d7c36
Oleg <oleg@theworkinggroup.ca>
2009-04-17 13:09:31 -0400

    frozen acts_as_tree

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
 require 'cms_rails_extensions'
+require 'acts_as_tree'
--- lib/cms_rails_extensions.rb
+++ lib/cms_rails_extensions.rb
@@ -1,8 +1,8 @@
 class Array
- def unique_by
-   inject(Hash.new(0)) do |h, i|
-     h[yield(i)] += 1
-     h
-   end.size == size
- end
+  def unique_by
+    inject(Hash.new(0)) do |h, i|
+      h[yield(i)] += 1
+      h
+    end.size == size
+  end
 end
