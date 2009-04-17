0a5a59c7976ee14dbfb7746c73a0a31e7142cebc
Oleg <oleg@theworkinggroup.ca>
2009-04-17 14:40:28 -0400

    added slugs to snippets and handy method to pull them in everywhere (#8)

--- app/models/cms_snippet.rb
+++ app/models/cms_snippet.rb
@@ -2,12 +2,21 @@ class CmsSnippet < ActiveRecord::Base
   
   # -- Validations ----------------------------------------------------------
   
-  validates_presence_of :label
-  validates_uniqueness_of :label
-  
+  validates_presence_of   :label, 
+                          :slug
+  validates_uniqueness_of :slug
+  validates_format_of     :slug,
+    :with   => /^\w[a-z0-9_-]*$/i
   
   # -- Scopes ---------------------------------------------------------------
   
   default_scope :order => 'position ASC'
+  
+  # -- Class Methods --------------------------------------------------------
+  
+  def self.content_for(slug)
+    s = find_by_slug(slug)
+    s.blank? ? "{{ #{slug} }}" : s.content
+  end
     
 end
