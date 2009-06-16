a432fbb49b680a899c16ac59daf4ff7cd7937848
Oleg <oleg@theworkinggroup.ca>
2009-06-16 15:57:35 -0400

    more generator stuff

--- generators/cms/cms_generator.rb
+++ generators/cms/cms_generator.rb
@@ -2,6 +2,13 @@ class CmsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'migrations/create_cms.rb', 'db/migrate', :migration_file_name => 'create_cms'
+      
+      sass_dir = Sass::Plugin.options[:template_location].gsub(Rails.root, '')
+      m.directory sass_dir
+      m.file "stylesheets/cms_master.sass", "#{sass_dir}/cms_master.sass", :collision => :skip
+      m.file 'javascripts/cms_mce_init.js', 'public/javascripts/cms_mce_init.js', :collision => :skip
+      
+      m.readme 'README'
     end
   end
 end
---
+++ generators/cms/templates/javascripts/cms_mce_init.js
@@ -0,0 +1,12 @@
+try{
+  tinyMCE.init({
+    mode : "specific_textareas",
+    editor_selector : "mceEditor",
+    theme : "advanced",
+    theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect,code",
+    theme_advanced_buttons2 : '',
+    theme_advanced_buttons3 : '',
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+  });
+}catch(e){}
