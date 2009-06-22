0d3eb649555ae695b4b5487443b0dc27a6f8916a
Oleg <oleg@theworkinggroup.ca>
2009-06-22 14:25:03 -0400

    Not having haml/sass stalls generator

--- generators/cms/cms_generator.rb
+++ generators/cms/cms_generator.rb
@@ -1,15 +1,16 @@
 class CmsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.migration_template 'migrations/create_cms.rb', 'db/migrate', :migration_file_name => 'create_cms'
       
-      if defined?(Sass)
-        sass_dir = Sass::Plugin.options[:template_location].gsub(Rails.root, '') 
-        m.directory sass_dir
-        m.file "stylesheets/cms_master.sass", "#{sass_dir}/cms_master.sass", :collision => :skip
-      else
-        puts "!!! HAML/SASS not installed!"
+      if !defined?(Sass)
+        puts '/!\ Error: Please make sure application has HAML/SASS'
+        return
       end
+      
+      m.migration_template 'migrations/create_cms.rb', 'db/migrate', :migration_file_name => 'create_cms'
+      sass_dir = Sass::Plugin.options[:template_location].gsub(Rails.root, '') 
+      m.directory sass_dir
+      m.file "stylesheets/cms_master.sass", "#{sass_dir}/cms_master.sass", :collision => :skip
       m.file 'javascripts/cms_mce_init.js', 'public/javascripts/cms_mce_init.js', :collision => :skip
       m.file 'initializers/paperclip.rb', 'config/initializers/paperclip.rb', :collision => :skip
       
