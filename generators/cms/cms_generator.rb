34f87cb2337e1d60ea11af6b658b30ca532ac5bf
Hesham <hesham@Hesham.(none)>
2009-06-22 14:34:06 -0400

    Show error message if Haml/SASS is missing.

--- generators/cms/cms_generator.rb
+++ generators/cms/cms_generator.rb
@@ -4,17 +4,17 @@ class CmsGenerator < Rails::Generator::Base
       
       if !defined?(Sass)
         puts '/!\ Error: Please make sure application has HAML/SASS'
-        return
+      else
+        m.migration_template 'migrations/create_cms.rb', 'db/migrate', :migration_file_name => 'create_cms'
+        sass_dir = Sass::Plugin.options[:template_location].gsub(Rails.root, '') 
+        m.directory sass_dir
+        m.file "stylesheets/cms_master.sass", "#{sass_dir}/cms_master.sass", :collision => :skip
+        m.file 'javascripts/cms_mce_init.js', 'public/javascripts/cms_mce_init.js', :collision => :skip
+        m.file 'initializers/paperclip.rb', 'config/initializers/paperclip.rb', :collision => :skip
+
+        m.readme 'README'
       end
       
-      m.migration_template 'migrations/create_cms.rb', 'db/migrate', :migration_file_name => 'create_cms'
-      sass_dir = Sass::Plugin.options[:template_location].gsub(Rails.root, '') 
-      m.directory sass_dir
-      m.file "stylesheets/cms_master.sass", "#{sass_dir}/cms_master.sass", :collision => :skip
-      m.file 'javascripts/cms_mce_init.js', 'public/javascripts/cms_mce_init.js', :collision => :skip
-      m.file 'initializers/paperclip.rb', 'config/initializers/paperclip.rb', :collision => :skip
-      
-      m.readme 'README'
     end
   end
 end
