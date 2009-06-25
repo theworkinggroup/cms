1eeacbb45ac1ff0f77ed905b4e9f6273ed70d05c
Georges Gabereau <georges@theworkinggroup.ca>
2009-06-25 13:00:05 -0400

    Auto slugification on page creation and editing!

--- generators/cms/cms_generator.rb
+++ generators/cms/cms_generator.rb
@@ -9,6 +9,7 @@ class CmsGenerator < Rails::Generator::Base
         sass_dir = Sass::Plugin.options[:template_location].gsub(Rails.root, '') 
         m.directory sass_dir
         m.file "stylesheets/cms_master.sass", "#{sass_dir}/cms_master.sass", :collision => :skip
+        m.file 'javascripts/cms_utilities.js', 'public/javascripts/cms_utilities.js', :collision => :skip
         m.file 'javascripts/cms_mce_init.js', 'public/javascripts/cms_mce_init.js', :collision => :skip
         m.file 'javascripts/cms_codemirror_init.js', 'public/javascripts/cms_codemirror_init.js', :collision => :skip
         m.file 'initializers/paperclip.rb', 'config/initializers/paperclip.rb', :collision => :skip
