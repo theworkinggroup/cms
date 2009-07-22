ea8b75f5a8cc18d3cf72959b361ae6b450506cbf
Georges Gabereau <georges@theworkinggroup.ca>
2009-07-22 14:18:14 -0400

    Change the index names for categorizations because the auto-generate index names are too long for MySQL.

--- generators/cms/templates/migrations/create_cms.rb
+++ generators/cms/templates/migrations/create_cms.rb
@@ -87,14 +87,16 @@ class CreateCms < ActiveRecord::Migration
       t.integer :cms_category_id
       t.integer :cms_page_id
     end
-    add_index :cms_page_categorizations, [:cms_category_id, :cms_page_id], :unique => true
+    add_index :cms_page_categorizations, [:cms_category_id, :cms_page_id], :unique => true, 
+      :name => 'index_page_categorizations_on_category_id_and_page_id' #auto-generated name was too long.
     
     
     create_table :cms_attachment_categorizations do |t|
       t.integer :cms_category_id
       t.integer :cms_attachment_id
     end
-    add_index :cms_attachment_categorizations, [:cms_category_id, :cms_attachment_id], :unique => true
+    add_index :cms_attachment_categorizations, [:cms_category_id, :cms_attachment_id], :unique => true,
+      :name => 'index_attach_categorizations_on_category_id_and_attachment_id' #auto-generated name was too long.
     
   end
   
