2bcdef9decc86f3ef5fd7438d497686a8660bcce
Hesham E <hesham@twg.ca>
2010-04-27 13:34:09 -0400

    fixed styles

--- generators/cms/templates/javascripts/utilities.js
+++ generators/cms/templates/javascripts/utilities.js
@@ -13,3 +13,29 @@ function slugify(str) {
   str = str.replace(/[^a-zA-Z0-9 -]/g, '').replace(/\s+/g, '-').toLowerCase();
   return str;
 }
+
+$(document).ready(function() {
+
+  // Expand/Collapse tree function
+  $('.tree_toggle').bind('click', function() {
+    $(this).siblings('ul').toggle();
+    $(this).toggleClass('closed');
+    element_id = $(this).parent().attr('id').split('_')[2];
+    element_type = $(this).parent().attr('title');
+    $.ajax({url: ['/cms-admin', element_type, element_id, 'toggle'].join('/')});
+  })
+  
+  // Show/hide details
+  $('.details_toggle').bind('click', function() {
+    $(this).parent().siblings('table.details').toggle();
+  })
+
+  // Sortable trees
+  $('.sortable').each(function(){
+    $(this).sortable({  handle: 'div.dragger', 
+                        update: function() {
+                          $.post('/cms-admin/layouts/reorder', '_method=put&'+$(this).sortable('serialize'));
+                        } 
+    })
+  })
+});
