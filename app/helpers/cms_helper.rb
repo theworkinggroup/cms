7c7bdfdb19cc909b9da67b6dcd92d96cfca3860e
Jamie Wong <jamie.lf.wong@gmail.com>
2010-08-11 11:31:37 -0400

    added formatted class for cms_form

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -14,12 +14,13 @@ module CmsHelper
   end
   
   def link_to_toggle(object, children_count)
-    link_to_function(children_count, "", :class => ['tree_toggle', tree_state(object)].join(' '), :title => 'Expand/Collapse', :onclick => "object_id = '#{object.id}'")
+    link_to_function(children_count, '', :class => ['tree_toggle', tree_state(object)].join(' '), :title => 'Expand/Collapse', :onclick => "object_id = '#{object.id}'")
   end
   
   def cms_form_for(record_or_name_or_array, *args, &proc)
     options = args.extract_options!
     options.merge!(:builder => CmsFormBuilder)    
+    (options[:html] ||= {}).merge!(:class => (options[:html][:class].to_s + ' formatted'))
     form_for(record_or_name_or_array, *(args << options), &proc)
   end
   
