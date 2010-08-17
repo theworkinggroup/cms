eab784fa099d851df8b714b87158dae2d51c34d6
Jamie Wong <jamie.lf.wong@gmail.com>
2010-08-17 13:13:03 -0400

    flag CmsSnippet.content_for as html_safe

--- app/models/cms_snippet.rb
+++ app/models/cms_snippet.rb
@@ -1,15 +1,15 @@
 class CmsSnippet < ActiveRecord::Base
-  
+
   # -- Validations ----------------------------------------------------------
   validates_presence_of   :label
   validates_uniqueness_of :label
   validates_format_of     :label,
     :with   => /^\w[a-z0-9_-]*$/i
-  
+
   # -- Class Methods --------------------------------------------------------
   def self.content_for(label)
     s = find_by_label(label)
-    s.blank? ? '' : s.content
+    s.blank? ? '' : s.content.html_safe
   end
-  
+
 end
